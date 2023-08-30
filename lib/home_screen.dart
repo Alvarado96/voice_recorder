99094a1f22bde8836b1b509828e5c599fd5a4c1e
Alvarado96 <ralph.1996.3@gmail.com>
2023-08-29 22:06:20 -0600

    Added a bottom app bar, no functionality yet just placeholders for now

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -6,6 +6,14 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(context) {
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: const Color.fromARGB(255, 2, 31, 70),
+        title: const Text(
+          "Voice Recorder",
+          textAlign: TextAlign.center,
+          style: TextStyle(color: Color.fromARGB(255, 179, 201, 213)),
+        ),
+      ),
       body: Center(
         child: SizedBox(
           width: double.infinity,
@@ -18,11 +26,41 @@ class HomeScreen extends StatelessWidget {
             ),
             child: const Column(
               mainAxisAlignment: MainAxisAlignment.center,
-              children: [Icon(Icons.mic, size: 100,)],
+              children: [
+                Icon(
+                  Icons.mic,
+                  size: 100,
+                )
+              ],
             ),
           ),
         ),
       ),
+      bottomNavigationBar: BottomAppBar(
+        shape: const CircularNotchedRectangle(),
+        color: const Color.fromARGB(255, 2, 31, 70),
+        child: IconTheme(
+            data: IconThemeData(color: Theme.of(context).colorScheme.onPrimary),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            IconButton(
+              tooltip: 'Home',
+              icon: const Icon(Icons.home_filled),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Record',
+              icon: const Icon(Icons.circle),
+              onPressed: () {},
+            ),
+            IconButton(
+              tooltip: 'Favorites',
+              icon: const Icon(Icons.favorite),
+              onPressed: () {},
+            )]),
+      ),
+      )
     );
   }
 }
