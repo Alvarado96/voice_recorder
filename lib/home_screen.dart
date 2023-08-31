ab36118168f821f5b06fd8145bad175cc6647188
Alvarado96 <ralph.1996.3@gmail.com>
2023-08-30 22:52:07 -0600

    changed the theme

--- lib/home_screen.dart
+++ lib/home_screen.dart
@@ -15,7 +15,9 @@ class HomeScreen extends StatelessWidget {
               children: [
                 IconButton(
                   icon: const Icon(Icons.mic),
-                  onPressed: () {},
+                  onPressed: () {
+                    
+                  },
                   iconSize: 100,
                 )
               ],
--- lib/voice_app.dart
+++ lib/voice_app.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:voice_recorder/routes/routes.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class VoiceApp extends StatefulWidget {
   const VoiceApp({super.key});
@@ -14,7 +15,13 @@ class _VoiceAppState extends State<VoiceApp> {
   @override
   Widget build(BuildContext context) {
     return MaterialApp.router(
-      
+      theme: ThemeData(
+        textTheme: GoogleFonts.robotoTextTheme(),
+        colorScheme: ColorScheme.fromSwatch(
+          primarySwatch: Colors.blueGrey
+          
+        )
+      ),
       title: "Voice Recorder",
       routerConfig: router,
     );
