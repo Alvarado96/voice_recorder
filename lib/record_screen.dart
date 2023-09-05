97b3db5dccbc028451a0326b0eb7b4d483cce14b
Alvarado96 <ralph.1996.3@gmail.com>
2023-09-04 20:33:57 -0600

    Changed the duration time from 0s format to 00:00 format

--- lib/record_screen.dart
+++ lib/record_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_sound/flutter_sound.dart';
 import 'package:permission_handler/permission_handler.dart';
+import 'dart:io';
 
 class RecordScreen extends StatefulWidget {
   const RecordScreen({super.key});
@@ -11,6 +12,7 @@ class RecordScreen extends StatefulWidget {
 
 class _RecordScreenState extends State<RecordScreen> {
   final recorder = FlutterSoundRecorder();
+  bool isRecorderReady = false;
   var isRecording = false;
   @override
   void initState() {
@@ -31,14 +33,22 @@ class _RecordScreenState extends State<RecordScreen> {
       throw "Microphone permission not granted";
     }
     await recorder.openRecorder();
+    isRecorderReady = true;
+    recorder.setSubscriptionDuration(const Duration(milliseconds: 500));
   }
 
   Future record() async {
+    if (!isRecorderReady) return;
     await recorder.startRecorder(toFile: 'audio');
   }
 
   Future stop() async {
-    await recorder.stopRecorder();
+    if (!isRecorderReady) return;
+
+    final path = await recorder.stopRecorder();
+    final audioFile = File(path!);
+
+    print('Recorded audio: $audioFile');
   }
 
   @override
@@ -50,6 +60,33 @@ class _RecordScreenState extends State<RecordScreen> {
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
+              StreamBuilder<RecordingDisposition>(
+                stream: recorder.onProgress,
+                builder: (context, snapshot) {
+                  final duration = snapshot.hasData
+                      ? snapshot.data!.duration
+                      : Duration.zero;
+                  
+                  String twoDigits(int n) => n.toString().padLeft(2, '0');
+                  final twoDigitMinutes = 
+                      twoDigits(duration.inMinutes.remainder(60));
+                  final twoDigitSeconds =
+                    twoDigits(duration.inSeconds.remainder(60));
+
+                  return Text(
+                    '$twoDigitMinutes:$twoDigitSeconds',
+                    style: const TextStyle(
+                      fontSize: 80,
+                      fontWeight: FontWeight.bold,
+                    ),
+                  );
+                  // return Text(
+                  //   '${duration.inSeconds} s',
+                  //   style: const TextStyle(
+                  //       fontSize: 80, fontWeight: FontWeight.bold),
+                  // );
+                },
+              ),
               Ink(
                 decoration: ShapeDecoration(
                     shape: const CircleBorder(),
